fc652047c3efd53e75f48664e5a29e7853b619e0
Roma <pestovr.gpt@gmail.com>
2024-11-14 12:21:43 +0300

    fix

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -1,35 +1,80 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+"use client";
+
+import { useState, useEffect } from "react";
 
 interface HeroSliderProps {
   images: string[];
 }
 
 const HeroSlider = ({ images }: HeroSliderProps) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  const goToNextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const goToPrevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
-    <Swiper
-      modules={[Pagination, Autoplay, Navigation]}
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      autoplay={{ delay: 3000 }}
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      loop={true}
-    >
+    <div className="relative w-full h-72 overflow-hidden">
       {images.map((img, index) => (
-        <SwiperSlide key={index}>
+        <div
+          key={index}
+          className={`absolute inset-0 transition-transform duration-700 ease-in-out ${
+            index === currentIndex ? "translate-x-0" : "translate-x-full"
+          }`}
+          style={{ transform: `translateX(${(index - currentIndex) * 100}%)` }}
+        >
           <img
             src={img}
             alt={`Wallpaper ${index + 1}`}
             className="w-full h-72 object-cover"
           />
-        </SwiperSlide>
+        </div>
       ))}
-    </Swiper>
+
+      <button
+        onClick={goToPrevSlide}
+        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full hover:bg-opacity-80 transition"
+      >
+        ❮
+      </button>
+
+      <button
+        onClick={goToNextSlide}
+        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full hover:bg-opacity-80 transition"
+      >
+        ❯
+      </button>
+
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrentIndex(index)}
+            className={`w-3 h-3 rounded-full ${
+              index === currentIndex ? "bg-white" : "bg-gray-500"
+            }`}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
