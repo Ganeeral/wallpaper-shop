349d2fc9e183b49ed722539a9082bfe62cd50a52
Roma <pestovr.gpt@gmail.com>
2024-11-14 12:09:33 +0300

    fix

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -1,7 +1,4 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 interface HeroSliderProps {
