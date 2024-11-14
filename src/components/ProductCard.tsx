fae86e6fc2e69f2d54a460dec1a35d44e255abd0
Roma <pestovr.gpt@gmail.com>
2024-11-14 12:29:28 +0300

    fix color

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,15 +10,17 @@ interface ProductProps {
   image: string;
   price: number;
   stock: number;
+  color: string;
 }
 
-const ProductCard = ({ id, name, image, price, stock }: ProductProps) => {
+const ProductCard = ({ id, name, image, price, stock, color }: ProductProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <div className="border p-4 rounded-lg">
       <img src={image} alt={name} className="h-48 w-full object-cover" />
       <h2 className="text-xl font-bold mt-4">{name}</h2>
       <p className="text-lg">Цена: {price}₽</p>
+      <p className="text-lg">Цвет: {color}₽</p>
       <Link href={`/product/${id}`}>
         <button className="bg-blue-600 text-white px-4 py-2 rounded mt-4">
           Подробнее
